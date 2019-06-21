5f9f67720b50bee77a02d75d04b9c8a4c9c13b94
valdo-git <52044744+valdo-git@users.noreply.github.com>
2019-06-21 13:47:07 +0000

    Add files via upload

---
+++ database/migrations/2019_06_19_142144_add_id_emplacementans_id_atelier_to_materiels.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddIdEmplacementansIdAtelierToMateriels extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('materiels', function (Blueprint $table) {
+            Schema::table('materiels', function (Blueprint $table) {
+            $table->integer('emplacement_id');
+            $table->integer('atelier_id');
+        });
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('materiels', function (Blueprint $table) {
+            $table->dropColumn('emplacement_id');
+            $table->dropColumn('atelier_id');
+        });
+    }
+}
