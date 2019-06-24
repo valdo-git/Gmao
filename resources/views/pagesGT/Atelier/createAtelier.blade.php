40ed6be3e4d9f3d663f2ed6ae3a66d97147d3666
valdo-git <52044744+valdo-git@users.noreply.github.com>
2019-06-24 13:57:17 +0000

    Add files via upload
    Modification des vues pour atelier

--- resources/views/pagesGT/Atelier/createAtelier.blade.php
+++ resources/views/pagesGT/Atelier/createAtelier.blade.php
@@ -1,6 +1,14 @@
-@extends('layouts.masterGT')
+@extends('adminlte::page')
 
-<title>Création atelier</title>
+@section('title', 'Créer atélier')
+
+@section('content_header')
+    <h3 class="box-title">Créer Atélier</h3>
+    <ol class="breadcrumb">
+        <li><a href="{{ route('homeGT') }}"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li class="active">Créer atélier</li>
+    </ol>
+    @stop
 
 @section('titre_page')
     <legend class="card-header" > Création Atelier</legend>
@@ -13,64 +21,45 @@
 
 @section('content')
 
+    <div class="box box-solid box-body">
 
+        <form class="form-horizontal" method="POST" action="{{ route('Ateliers.store') }}" accept-charset="UTF-8">
+            {!! csrf_field() !!}    
 
-<form method="POST"  id="form-ops" action="{{ route('Ateliers.store') }}" >
-    <div class="modal-body">
-        {!! csrf_field() !!}
-        <div class="row">
-            <div class="col-sm">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="input-group mb-3">
-                            <div class="input-group-append">
-                                <span class="input-group-text" id="basic-addon3">*Nom de l'atelier : </span>
-                            </div>
-                            <input type="text" class="form-control{{ $errors->has('nom_atelier') ? ' is-invalid' : '' }}" name="nom_atelier" id="nom_atelier"  required>
-                            @if ($errors->has('nom_atelier'))
-                                <span class="invalid-feedback">
-                                    <strong>{{ $errors->first('nom_atelier') }}</strong>
-                                </span>
-                            @endif
-                        </div>
-
-                        <div class="input-group mb-3">
-                            <div class="input-group-append">
-                                <span class="input-group-text" id="basic-addon3">*Grade et nom du chef d'atelier : </span>
-                            </div>
-                            <select  name="nom_chef" id="nom_chef" onselect="" class="custom-select form-control" required >
-                                <option>--  --</option>
-                                @foreach($listeChefAt as $chef)
-                                    <option value="{{$chef->grade.' '.$chef->name}}">{{$chef->grade}} {{$chef->name}}</option>
-                                @endforeach
-                            </select>
-                            @if ($errors->has('nom_chef'))
-                                <span class="invalid-feedback">
-                                    <strong>{{ $errors->first('nom_chef') }}</strong>
-                                </span>
-                            @endif
-                        </div>
-
-                        <div class="input-group mb-3">
-                            <div class="input-group-append">
-                                <span class="input-group-text" id="basic-addon3">Date de création : </span>
-                            </div>
-                            <input type="date" class="form-control" name="date_creation" id="date_creation">
-
-
-                        </div>
-
-
-                    </div>
+            <div class="form-group">
+                <label class="col-sm-4 text-right control-label col-form-label" >*Nom atélier:  </label>
+                <div class="col-sm-4">
+                    <input class="form-control{{ $errors->has('nom_atelier') ? ' is-invalid' : '' }}" name="nom_atelier" type="text" id="nom_atelier" required>
+                    @if ($errors->has('nom_atelier'))
+                        <span class="invalid-feedback bg-danger">
+                            <strong>{{ $errors->first('') }}</strong>
+                        </span>
+                    @endif
                 </div>
             </div>
-        </div>
-    </div>
-    <div class="modal-footer">
-        <button type="submit" class="btn btn-primary" name="newop">Enregistrer</button>
-        <button type="reset" class="btn btn-secondary" data-dismiss="modal">Annuler</button>
-    </div>
-</form>
+
+                <div class="form-group">
+                    <label class="col-sm-4 text-right control-label col-form-label" >*Grade et nom du chef d'atelier :  </label>
+                    <div class="col-sm-4">
+                        <input class="form-control" name="nom_chef" type="text" id="nom_chef" required>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="col-sm-4 text-right control-label col-form-label" >Date de création:  </label>
+                    <div class="col-sm-4">
+                        <input class="form-control" name="date_creation" type="date" id="date_creation" required>
+                    </div>
+                </div>
+
+                <div class="card-footer col-sm-8">
+                    <button class="btn btn-primary pull-right " type="submit" ><i class="fa fa-check-circle-o"></i>&nbsp; Valider l'enregistrement</button>
+                    <button class="btn" type="reset" ><i class="fa fa-reddit"></i>&nbsp; Annuler l'enregistrement</button>
+                </div>
 
 
-    @endsection
+        </form>
+                        
+    </div>
+
+@endsection
